8a93faeb4d197342efd4bbf000b25b93ff4b4378
MrMeeb <16963756+MrMeeb@users.noreply.github.com>
2025-01-24 12:11:30 +0000

    Develop source

--- Detection-Sample/detect.ps1
+++ Detection-Sample/detect.ps1
@@ -4,7 +4,7 @@ $LOGROOT="${env:ProgramFiles}\CAW\IntuneLogs\$AppName"
 
 Start-Transcript -path $LOGROOT\detect.ps1.log -append
 
-Invoke-Webrequest -uri https://raw.githubusercontent.com/MrMeeb/Intune-Winget-Universal-App-Installer/refs/heads/main/Online/detect.ps1 -outfile detect.ps1
+Invoke-Webrequest -uri https://raw.githubusercontent.com/MrMeeb/Intune-Winget-Universal-App-Installer/refs/heads/develop/Online/detect.ps1 -outfile detect.ps1
 powershell.exe -executionpolicy bypass .\detect.ps1 -AppName $AppName | Out-Host
 # Detect exit code of child script and exit with it if it isn't 0
 if ($LastExitCode -ne 0)
