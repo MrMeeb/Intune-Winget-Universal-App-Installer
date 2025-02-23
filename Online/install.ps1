b97b4e0dd3dfca46d4d686b380ca48ec5fd60d68
MrMeeb <16963756+MrMeeb@users.noreply.github.com>
2025-02-23 00:24:31 +0000

    Add logic for installing as user

--- Online/install.ps1
+++ Online/install.ps1
@@ -10,40 +10,69 @@ $LOGROOT="${env:ProgramFiles}\CAW\IntuneLogs\$AppName"
 
 Start-Transcript -path $LOGROOT\install.ps1.log -append
 
-# Locate WinGet
-$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_*__8wekyb3d8bbwe\winget.exe"
-if ($ResolveWingetPath){
-		$WingetPath = $ResolveWingetPath[-1].Path
-}
+if ( $(whoami) -like "*system*" ) {
 
-Write-Host $WingetPath
+    Write-Host "Running as System"
 
-if ($WingetPath.length -lt 10){
-	Write-Error -Message "Winget not found." -Category OperationStopped
-	exit
-}
+	# Locate WinGet
+	$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_*__8wekyb3d8bbwe\winget.exe"
+	if ($ResolveWingetPath){
+			$WingetPath = $ResolveWingetPath[-1].Path
+	}
 
-$WingetPath = Split-Path -Path $WingetPath -Parent
+	Write-Host $WingetPath
 
-switch ($Action){
-    "install" {
-		try {
-			Set-Location $WingetPath
-			.\winget.exe install --exact --id $AppName --silent --accept-package-agreements --accept-source-agreements
+	if ($WingetPath.length -lt 10){
+		Write-Error -Message "Winget not found." -Category OperationStopped
+		exit
+	}
+
+	$WingetPath = Split-Path -Path $WingetPath -Parent
+
+	switch ($Action){
+		"install" {
+			try {
+				Set-Location $WingetPath
+				.\winget.exe install --exact --id $AppName --silent --accept-package-agreements --accept-source-agreements
+			}
+			catch {
+				Write-Error -Message "Error happened during installation." -Category OperationStopped
+			}
 		}
-		catch {
-			Write-Error -Message "Error happened during installation." -Category OperationStopped
+		"uninstall" {
+			try {
+				Set-Location $WingetPath
+				.\winget.exe uninstall --exact --id $AppName --silent
+			}
+			catch {
+				Write-Error -Message "Error happened during uninstallation." -Category OperationStopped
+			}
 		}
 	}
-    "uninstall" {
-		try {
-			Set-Location $WingetPath
-			.\winget.exe uninstall --exact --id $AppName --silent
+
+} else {
+	
+	Write-Host "Running as User"
+	
+	switch ($Action){
+		"install" {
+			try {
+				winget install --exact --id $AppName --silent --accept-package-agreements --accept-source-agreements
+			}
+			catch {
+				Write-Error -Message "Error happened during installation." -Category OperationStopped
+			}
 		}
-		catch {
-			Write-Error -Message "Error happened during uninstallation." -Category OperationStopped
-    	}
+		"uninstall" {
+			try {
+				winget uninstall --exact --id $AppName --silent
+			}
+			catch {
+				Write-Error -Message "Error happened during uninstallation." -Category OperationStopped
+			}
+		}
 	}
+
 }
 
 Stop-Transcript
